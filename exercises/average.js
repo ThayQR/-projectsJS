c7845bdbf23ef2dae83a374f931371ba77d1b8da
ThayQR <62308898+ThayQR@users.noreply.github.com>
2020-11-23 11:39:12 -0300

    add  average

---
+++ exercises/average.js
@@ -0,0 +1,11 @@
+nota1 = prompt("informe a primeira nota")
+nota2 = prompt("informe a segunda nota")
+
+ let soma = nota1 + nota2
+ let media = soma/2
+
+ if(media >= 7){
+   alert("Aprovado")
+ }else{
+   alert("Reprovado")
+ }
